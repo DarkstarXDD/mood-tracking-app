3efd9c7611dd36391f473198e678f20079ded7b6
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-05-06 10:04:29 +0530

    chore: initialize project and add meta tags

---
+++ README.md
@@ -0,0 +1,38 @@
+## Frontend Mentor Challenge 32 - Mood Tracking App
+
+This is my solution to the [Mood Tracking App](https://www.frontendmentor.io/challenges/mood-tracking-app-E2XeKhDF0B) challenge on [Frontend Mentor](https://www.frontendmentor.io/).
+
+#
+
+### Links 🔗
+
+- Live Site URL: https://mood-tracking-app-darkstarxdd.vercel.app/
+
+#
+
+### Built with 🔨
+
+- Content
+
+#
+
+### Features ✨
+
+- Content.
+
+#
+
+### Technical Details 🔧
+
+- Content.
+
+#
+
+### Screenshots of the Solution (Desktop & Mobile) 🔍
+
+![](./solution_screenshots/screenshot_desktop.jpeg)
+![](./solution_screenshots/screenshot_mobile.jpeg)
+
+#
+
+- My Frontend Mentor profile - [@DarkstarXDD](https://www.frontendmentor.io/profile/DarkstarXDD)
---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+import { Reddit_Sans } from "next/font/google"
+
+import "./globals.css"
+
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Mood Tracking App",
+  description:
+    "Check out Darkstar's solution for the Mood Tracking App challenge on Frontend Mentor",
+
+  authors: {
+    name: "Darkstar",
+    url: "https://github.com/DarkstarXDD",
+  },
+
+  openGraph: {
+    type: "website",
+    url: "https://mood-tracking-app-darkstarxdd.vercel.app/",
+    title: "Mood Tracking App",
+
+    description:
+      "Check out Darkstar's solution for the Mood Tracking App challenge on Frontend Mentor",
+
+    images: {
+      url: "https://mood-tracking-app-darkstarxdd.vercel.app/og-img.jpeg",
+      type: "image/jpeg",
+      width: 1440,
+      height: 756,
+    },
+  },
+}
+
+const redditSans = Reddit_Sans({
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+  variable: "--font-reddit-sans",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${redditSans.variable} font-reddit-sans bg-zinc-800 text-white`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
