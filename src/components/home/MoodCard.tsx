34007e180cfe7270bceeaf1c7cc9b0c7352edb28
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-05-19 10:39:38 +0530

    style: update mood card layout

--- src/components/home/MoodCard.tsx
+++ src/components/home/MoodCard.tsx
@@ -9,7 +9,7 @@ import veryHappyEmoji from "@/assets/icon-very-happy-color.svg"
 
 export default function MoodCard() {
   return (
-    <div className="grid justify-items-center gap-8 rounded-2xl border border-blue-100 bg-white px-4 py-8 md:justify-items-start md:px-8 lg:row-span-2">
+    <div className="grid justify-items-center gap-8 overflow-hidden rounded-2xl border border-blue-100 bg-white px-4 py-8 md:grid-cols-[1fr_auto] md:justify-items-start md:px-8 lg:row-span-2">
       <h2 className="flex flex-col justify-start text-center font-bold tracking-tight text-neutral-900 md:text-start">
         <span className="text-4xl leading-normal opacity-70">
           I&apos;m feeling
@@ -17,9 +17,13 @@ export default function MoodCard() {
         <span className="text-5xl leading-tight">Very Happy</span>
       </h2>
 
-      <Image src={veryHappyEmoji} alt="" className="h-50 w-50" />
+      <Image
+        src={veryHappyEmoji}
+        alt=""
+        className="row-span-2 h-50 w-50 md:h-80 md:w-80 md:translate-y-12 md:justify-self-end"
+      />
 
-      <div className="grid justify-items-center gap-4 text-center md:justify-items-start md:gap-3 md:text-start">
+      <div className="flex flex-col items-center gap-4 text-center md:items-start md:justify-items-start md:gap-3 md:self-end md:text-start">
         <ImQuotesLeft className="size-6 text-blue-600" />
         <blockquote className="text-lg leading-snug font-medium tracking-normal text-neutral-900 italic">
           “When your heart is full, share your light with the world.”
