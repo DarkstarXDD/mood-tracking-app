5f66bbd47689471e3194f2171fc2e145f5a6f142
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-05-25 20:42:36 +0530

    feat: error boundary ui

---
+++ src/app/error.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Error() {
+  return (
+    <div className="grid grid-cols-[auto_1fr] items-center gap-4">
+      <Image
+        src="/assets/images/icon-sad-color.svg"
+        alt=""
+        width={154}
+        height={154}
+        className="row-span-2 size-16"
+      />
+      <h1 className="text-2xl leading-normal font-semibold tracking-normal text-neutral-900">
+        Something went wrong!
+      </h1>
+      <p className="col-start-2 text-base leading-normal font-normal tracking-tight text-neutral-600">
+        <span>
+          Your last action did not complete properly. Please go back to the home
+          page and try agin.{" "}
+        </span>
+        <Link href="/" className="underline">
+          Back to home
+        </Link>
+      </p>
+    </div>
+  )
+}
