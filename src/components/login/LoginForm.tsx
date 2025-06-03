5f35848dcb4d3f8ce4077d77a8acc96268c8aadd
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-06-03 07:51:07 +0530

    fix: git not tracking filename case changes 2

---
+++ src/components/login/LoginForm.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import Link from "next/link"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+
+import { loginUser } from "@/actions/auth"
+import Button from "@/components/ui/Button"
+import LoadingDots from "@/components/ui/LoadingDots"
+import TextField from "@/components/ui/TextField"
+import { loginSchema } from "@/lib/schema"
+
+import type { LoginSchemaType } from "@/lib/schema"
+
+export default function LoginForm() {
+  const [status, setStatus] = useState<"idle" | "loading">("idle")
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<LoginSchemaType>({
+    resolver: zodResolver(loginSchema),
+  })
+
+  return (
+    <form
+      className="shadow-main grid w-full max-w-lg gap-8 rounded-2xl bg-white px-4 py-10 md:px-8"
+      onSubmit={handleSubmit(async (formData) => {
+        setStatus("loading")
+        const response = await loginUser(formData)
+        if (response) {
+          setError("email", response)
+          setStatus("idle")
+        }
+      })}
+    >
+      <div className="grid gap-2">
+        <h1 className="text-4xl leading-normal font-bold tracking-tight text-neutral-900">
+          Welcome back!
+        </h1>
+        <p className="text-lg leading-normal tracking-tight text-neutral-600">
+          Log in to continue tracking your mood and sleep.
+        </p>
+      </div>
+
+      <fieldset
+        className="grid gap-5 disabled:opacity-60"
+        disabled={status === "loading"}
+      >
+        <TextField
+          label="Email address"
+          autoComplete="email"
+          {...register("email")}
+          errorMessage={errors.email?.message}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          {...register("password")}
+          errorMessage={errors.password?.message}
+        />
+      </fieldset>
+
+      <div className="grid gap-5">
+        <Button type="submit" isDisabled={status === "loading"}>
+          {status === "idle" ? "Log In" : <LoadingDots />}
+        </Button>
+
+        <p className="text-center text-lg leading-normal tracking-tight">
+          <span className="text-neutral-600">
+            Haven&apos;t got an account?{" "}
+          </span>
+          <Link
+            href="/signup"
+            className="text-blue-600 underline hover:text-blue-700"
+          >
+            Sign up.
+          </Link>
+        </p>
+      </div>
+    </form>
+  )
+}
