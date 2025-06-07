fd698afb6dd05c03c6cd8425fd532531e9aece1e
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2025-06-07 20:50:56 +0530

    refactor: remove unwanted average mood and sleep components

--- src/components/home/checkin-summary/AverageMoodCard.tsx
+++ src/components/home/checkin-summary/AverageMoodCard.tsx
@@ -1,7 +1,20 @@
-import AverageMood from "@/components/home/checkin-summary/AverageMood"
 import NotEnoughData from "@/components/home/checkin-summary/NotEnoughData"
+import SVGIcon from "@/components/ui/SVGIcon"
+import useMoodFormOptions from "@/hooks/useMoodFormOptions"
 import useUser from "@/hooks/useUser"
+import { averageDiffToTextMap, averageDiffToIconMap } from "@/lib/data-maps"
 import { getAverageMoodData } from "@/lib/helpers/mood-helpers"
+import { cn } from "@/lib/utils"
+
+import type { SVGIconNameType } from "@/lib/types"
+
+const moodIdToColorMap: Record<number, string> = {
+  1: "bg-red-300",
+  2: "bg-indigo-200",
+  3: "bg-blue-300",
+  4: "bg-green-300",
+  5: "bg-amber-300",
+}
 
 export default function AverageMoodCard() {
   const {
@@ -11,6 +24,9 @@ export default function AverageMoodCard() {
 
   const { averageMoodId, averageMoodWeightDiff } =
     getAverageMoodData(moodEntries)
+
+  const { moods } = useMoodFormOptions()
+  const Icon = averageDiffToIconMap[String(averageMoodWeightDiff)]
 
   return (
     <div className="grid gap-3">
@@ -23,13 +39,34 @@ export default function AverageMoodCard() {
         </span>
       </h2>
 
-      {hasFiveEntries ? (
-        <AverageMood
-          averageMoodId={averageMoodId}
-          averageMoodWeightDiff={averageMoodWeightDiff}
-        />
-      ) : (
-        <NotEnoughData type="mood" />
+      {!hasFiveEntries && <NotEnoughData type="mood" />}
+
+      {hasFiveEntries && (
+        <div
+          className={cn(
+            "custom-bg flex min-h-38 flex-col items-start justify-center gap-3 rounded-2xl px-4 py-5 md:px-5",
+            moodIdToColorMap[averageMoodId]
+          )}
+        >
+          <div className="flex items-center gap-3 lg:gap-4">
+            <SVGIcon
+              name={
+                moods.find((m) => m.id === averageMoodId)
+                  ?.iconWhite as SVGIconNameType
+              }
+              className="size-7.5"
+            />
+            <p className="text-2xl leading-normal font-semibold tracking-normal text-neutral-900">
+              {moods.find((m) => m.id === averageMoodId)?.label}
+            </p>
+          </div>
+          <div className="flex items-center gap-2">
+            <Icon className="size-4 text-neutral-900" />
+            <p className="text-base leading-normal font-normal tracking-tight text-neutral-900">
+              {averageDiffToTextMap[String(averageMoodWeightDiff)]}
+            </p>
+          </div>
+        </div>
       )}
     </div>
   )
--- src/components/home/checkin-summary/AverageSleepCard.tsx
+++ src/components/home/checkin-summary/AverageSleepCard.tsx
@@ -1,6 +1,8 @@
-import AverageSleep from "@/components/home/checkin-summary/AverageSleep"
 import NotEnoughData from "@/components/home/checkin-summary/NotEnoughData"
+import SVGIcon from "@/components/ui/SVGIcon"
+import useMoodFormOptions from "@/hooks/useMoodFormOptions"
 import useUser from "@/hooks/useUser"
+import { averageDiffToTextMap, averageDiffToIconMap } from "@/lib/data-maps"
 import { getAverageMoodData } from "@/lib/helpers/mood-helpers"
 
 export default function AverageSleepCard() {
@@ -11,6 +13,9 @@ export default function AverageSleepCard() {
 
   const { averageSleepId, averageSleepWeightDiff } =
     getAverageMoodData(moodEntries)
+
+  const { hoursOfSleep } = useMoodFormOptions()
+  const Icon = averageDiffToIconMap[String(averageSleepWeightDiff)]
 
   return (
     <div className="grid gap-3">
@@ -23,13 +28,26 @@ export default function AverageSleepCard() {
         </span>
       </h2>
 
-      {hasFiveEntries ? (
-        <AverageSleep
-          averageSleepId={averageSleepId}
-          averageSleepWeightDiff={averageSleepWeightDiff}
-        />
-      ) : (
-        <NotEnoughData type="sleep" />
+      {!hasFiveEntries && <NotEnoughData type="sleep" />}
+
+      {hasFiveEntries && (
+        <div className="custom-bg flex min-h-38 flex-col items-start justify-center gap-3 rounded-2xl bg-blue-600 px-4 py-5 md:px-5">
+          <div className="flex items-center gap-3 lg:gap-4">
+            <SVGIcon
+              name="icon-sleep"
+              className="size-6 text-white opacity-70"
+            />
+            <p className="text-2xl leading-normal font-semibold tracking-normal text-white">
+              {hoursOfSleep.find((hour) => hour.id === averageSleepId)?.label}
+            </p>
+          </div>
+          <div className="flex items-center gap-2">
+            <Icon className="size-4 text-white opacity-70" />
+            <p className="text-base leading-normal font-normal tracking-tight text-white opacity-70">
+              {averageDiffToTextMap[String(averageSleepWeightDiff)]}
+            </p>
+          </div>
+        </div>
       )}
     </div>
   )
